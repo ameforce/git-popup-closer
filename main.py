e8a4caa68c081bf963abbf7c4facf44218fbcc10
jongin.kim <enmsoftware@gmail.com>
2022-09-06 16:52:23 +0900

    feat: implement account manage
    Completed the implementation of the part that manages the account.

--- main.py
+++ main.py
@@ -10,23 +10,59 @@ class AccountManagement:
         self.path = r'SOFTWARE\ENMSoft\GPC'
         self.account_key_name = 'account_data'
         self.ascii_check_range = 126
+        self.rand_range = 128
         self.account_data = ''
 
     def encryption_data(self, resource: str = None):
         encrypted_resource = []
-        for char in resource:
+        id_resource, pw_resource = resource.split('@Account Separator#')
+
+        for char in id_resource:
             char = char + '@enm#'
             encrypted_char = []
+            rand_num = random.randint(1, self.rand_range)
             for semi_char in char:
-                semi_char = chr(ord(semi_char) + random.randint(1, 128))
+                semi_char = chr(ord(semi_char) + rand_num)
+                encrypted_char.append(semi_char)
+            encrypted_resource.append(''.join(encrypted_char))
+
+        encrypted_resource.append('@Account Separator#')
+
+        for char in pw_resource:
+            char = char + '@enm#'
+            encrypted_char = []
+            rand_num = random.randint(1, self.rand_range)
+            for semi_char in char:
+                semi_char = chr(ord(semi_char) + rand_num)
                 encrypted_char.append(semi_char)
             encrypted_resource.append(''.join(encrypted_char))
         self.account_data = ''.join(encrypted_resource)
 
     def decryption_data(self):
+        decrypted_account = []
+        for resource in self.account_data.split('@Account Separator#'):
+            new_resource = []
+            for i in range(len(resource) // 6):
+                split_data = []
+                for j in range(6):
+                    split_data.append(resource[i*6+j])
+                new_resource.append(split_data)
 
-        for char in self.account_data:
-
+            for raw_data in new_resource:
+                max_range = self.rand_range
+                raw_data_min_num = min(ord(raw_char) for raw_char in raw_data[0:6])
+                if raw_data_min_num < self.rand_range:
+                    max_range = raw_data_min_num
+                for i in range(max_range):
+                    decrypting_data = []
+                    for raw_char in raw_data:
+                        decrypting_data.append(chr(ord(raw_char) - i))
+                    if ''.join(decrypting_data[1:6]) == '@enm#':
+                        decrypted_account.append(decrypting_data[0])
+                        break
+            if '@Account Separator#' not in decrypted_account:
+                decrypted_account.append('@Account Separator#')
+        return ''.join(decrypted_account)
 
     def check_validity(self, resource: str = None):
         for char in resource:
@@ -34,20 +70,24 @@ class AccountManagement:
                 return False
         return True
 
-    def input_account_data(self, fall_back_state: bool = False):
-        if not fall_back_state:
+    def input_account_data(self):
+        id_resource, pw_resource = '', ''
+        while True:
             id_resource = input('Enter ID to use for automatic login: ')
-            if self.check_validity(id_resource):
-                pw_resource = input('Enter PW to use for automatic login: ')
-                if self.check_validity(pw_resource):
-                    self.encryption_data(f'@id_start#{id_resource}@id_end#@pw_start#{pw_resource}@pw_end#')
-                    return True
-                else:
-                    self.input_account_data(True)
-        else:
-            print('Only English, numbers, and special characters are allowed. Please re-enter.')
+            if not self.check_validity(id_resource):
+                print('\nOnly English, numbers, and special characters are allowed. Please re-enter.')
+                continue
+            break
+
+        while True:
             pw_resource = input('Enter PW to use for automatic login: ')
-            return self.check_validity(pw_resource)
+            if not self.check_validity(pw_resource):
+                print('\nOnly English, numbers, and special characters are allowed. Please re-enter.')
+                continue
+            break
+
+        self.encryption_data(f'{id_resource}@Account Separator#{pw_resource}')
+        self.save_account()
 
     def read_account(self):
         reg_handle = winreg.ConnectRegistry(None, self.hkey_type)
@@ -55,9 +95,10 @@ class AccountManagement:
             reg_key = winreg.OpenKey(reg_handle, self.path, 0, winreg.KEY_READ)
             value = winreg.QueryValueEx(reg_key, self.account_key_name)
             winreg.CloseKey(reg_key)
-            return True, value
+            self.account_data = value[0]
+            return True
         except WindowsError:
-            return False, None
+            return False
 
     def save_account(self, fall_back_state: bool = False):
         reg_handle = winreg.ConnectRegistry(None, self.hkey_type)
@@ -76,9 +117,9 @@ class AccountManagement:
 
 def main():
     am = AccountManagement()
-    am.input_account_data()
-    print(am.account_data)
-    # print(am.account_data)
+    if not am.read_account():
+        am.input_account_data()
+    print(am.decryption_data())
 
 
 if __name__ == '__main__':
